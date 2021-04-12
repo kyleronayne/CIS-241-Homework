32c48d3d54299ed133939a37722511ff96751334
Kyle Ronayne <kyleronaynep@gmail.com>
2021-04-12 13:37:34 -0400

    Update main.c

--- assignments/sorting_project/main.c
+++ assignments/sorting_project/main.c
@@ -14,7 +14,7 @@ int main(int argc, char** argv) {
 	char* sortedFileName = argv[2];
 
 	char* contents;
-	size_t fileSize = load_file(unsortedFileName, &contents);
+	size_t fileSize = fopen(unsortedFileName, "r");
 
 	int numWords = 0;
 	for (int i = 0; i < fileSize; i++) {
@@ -25,7 +25,7 @@ int main(int argc, char** argv) {
 	
 	sort(&contents, numWords);
 
-	save_file(sortedFileName, contents, fileSize);
+	fprintf(sortedFileName, contents, fileSize);
 	free(contents);
 	return 0;
 }
