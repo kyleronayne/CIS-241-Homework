9e730d6ffded6b5f30c8b1c39ce322a8dd91f99a
Kyle Ronayne <kyleronaynep@gmail.com>
2021-04-14 21:20:07 -0400

    Finishes sorter.c comments

--- assignments/sorting_project/sorter.c
+++ assignments/sorting_project/sorter.c
@@ -74,11 +74,23 @@ void quicksort(char** contents, int left, int right) {
 	quicksort(contents, pivotIndex + 1, right);
 }
 
+
+/***********************************************************************
+A function that receives a string of words and sorts them.
+@param char** contents: A pointer to a string of newline separated words
+@param int size: The number of words in the contents string
+@return: None
+***********************************************************************/
 void sort(char** contents, int size) {
+
+	// The number of words in contents
 	int numWords = size;
 	int maxWordLength = 0;
+
+	// The number of bytes of contents
 	int numBytes = 0;
 
+	// Determining the length of the longest word
 	int tempMaxWordLength = 0;
 	for (int i = 0; i < numWords; i++) {
 		while ((*contents)[numBytes++] != '\n') {
@@ -90,17 +102,20 @@ void sort(char** contents, int size) {
 		tempMaxWordLength = 0;
 	}
 
+	// Creating an array of the appropraite size and placing the
+	// words into it
 	char** list;
 	list = (char **)malloc(numWords * sizeof(char*));
 	for (int i = 0; i < numWords; i++) {
 		list[i] = (char*)malloc(maxWordLength * sizeof(char));
 	}
-
 	int a = 0;
 	int b = 0;
 	for (int i = 0; i < numBytes; i++) {
 		if((*contents)[i] == '\n') {
-			// set to null character
+			// Set to null character, we do not want to
+			// inlcude a newline character as part of the
+			// string
 			list[a][b] = '\0';
 			a++;
 			b = 0;
@@ -109,8 +124,10 @@ void sort(char** contents, int size) {
 		}
 	}
 
+	// Using the quicksort function to sort the array of strings
 	quicksort(list, 0, numWords - 1);
 
+	// Transforming the array back into a string
 	char* sortedContents = malloc(numBytes);
 	strcpy(sortedContents, "");
 	for (int i = 0; i < numWords; i++) {
@@ -122,6 +139,10 @@ void sort(char** contents, int size) {
 	for (int i = 0; i < numWords; i++) {
 		free(list[i]);
 	}
+
+	// Deallocating list from memory
 	free(list);
+
+	// Deallocating sortedContents
 	free(sortedContents);
 }
